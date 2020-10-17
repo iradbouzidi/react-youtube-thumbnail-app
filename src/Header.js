5f713f693c660a5e9dd01ca2dd5856f6f7cc6aae
iradbouzidi <irad.bouzidi@esprit.tn>
2020-10-17 23:50:30 +0100

    finished

--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,43 @@
 import React from "react";
 import "./Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import TextField from "@material-ui/core/TextField";
+import SearchIcon from "@material-ui/icons/Search";
+import Button from "@material-ui/core/Button";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import AppsIcon from "@material-ui/icons/Apps";
+import VideoCallIcon from "@material-ui/icons/VideoCall";
 
 function Header() {
   return (
     <div className="header">
-      <div className="header__logo">
-        <h1>Logo</h1>
+      <div className="header__leftSide">
+        <MenuIcon  />
+        <img 
+          className="header__logo" 
+          src="https://1000logos.net/wp-content/uploads/2017/05/Youtube-Logo.png"
+          alt=""
+        />
       </div>
+
       <div className="header__search">
-        <input type="text" />
-        <button>Icon</button>
+        <TextField
+          className="header__input"
+          placeholder="Search"
+          variant="outlined"
+        />
+        <Button className="header__searchButton"
+          variant="outlined">
+          <SearchIcon />
+        </Button>
       </div>
-      <div className="header__rightlogos">
-        <h1>Right logos</h1>
+      
+      <div className="header__rightside">
+        <VideoCallIcon />
+        <AppsIcon />
+        <NotificationsIcon />
       </div>
+    
     </div>
   );
 }
